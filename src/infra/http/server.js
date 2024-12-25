68ab19a6f24b69ea3f3734099ebe592723b1a86e
Eraldo Sinicio <eraldosinicio@gmail.com>
2024-12-25 12:00:05 -0300

    refact: move config env values

--- src/infra/http/server.js
+++ src/infra/http/server.js
@@ -1,15 +1,10 @@
-import dotenv from 'dotenv'
 import '@babel/polyfill'
 import Koa from 'koa'
 import { RouterApp } from './route'
-
-const envName = process.env.NODE_ENV
-dotenv.config({ path: `.env.${envName}` })
+import { ENV_NAME, SERVER_PORT } from '../config'
 
 const app = new Koa()
 
-const port = process.env.SERVER_PORT
-
 app.use(RouterApp.routes())
 
-app.listen(port, () => console.log(`🔥️ Servidor ${envName.toUpperCase()} Rodando!`))
+app.listen(SERVER_PORT, () => console.log(`🔥️ Servidor ${ENV_NAME.toUpperCase()} Rodando!`))
