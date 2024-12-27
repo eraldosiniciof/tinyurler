034b46eb43cc21486d8a165f4b4b15fbbad73899
Eraldo Sinicio <eraldosinicio@gmail.com>
2024-12-27 14:50:53 -0300

    feat: swagger added

--- src/infra/http/route.js
+++ src/infra/http/route.js
@@ -1,9 +1,21 @@
 import Router from '@koa/router'
 import bodyParser from 'koa-bodyparser'
 
-import { urlsRoutes } from '@modules/urls/infra/http/routes/urls'
+import { koaSwagger } from 'koa2-swagger-ui'
+import swaggerDocs from '../../../swaggerConfig' // Importe a configuração do Swagger
 
+import { urlsRoutes } from '@modules/urls/infra/http/routes/urls'
+1
 const RouterApp = new Router()
+
+RouterApp.get(
+  '/docs',
+  koaSwagger({
+    swaggerOptions: {
+      spec: swaggerDocs
+    }
+  })
+)
 
 RouterApp.use(bodyParser())
 RouterApp.use(urlsRoutes.routes())
