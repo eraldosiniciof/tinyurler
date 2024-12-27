cc111f1f06fa1bb21d722124c9e6316a73c22b7a
Eraldo Sinicio <eraldosinicio@gmail.com>
2024-12-27 14:51:15 -0300

    chore: add swagger doc to urls endpoints

--- src/modules/urls/infra/http/routes/urls.js
+++ src/modules/urls/infra/http/routes/urls.js
@@ -5,8 +5,55 @@ import { getShortenedUrlFactory } from '@modules/urls/useCases/GetShortenedUrl/G
 
 const urlsRoutes = new Router()
 
+/**
+ * @swagger
+ * /:
+ *   post:
+ *     summary: "Cria um URL encurtado"
+ *     description: "Recebe um URL original e retorna um URL encurtado."
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               original:
+ *                 required: true
+ *                 type: string
+ *                 description: "URL original a ser encurtado"
+ *                 example: "https://www.exemplo.com"
+ *     responses:
+ *       201:
+ *         description: "URL encurtado gerado com sucesso"
+ *       500:
+ *         description: "Parâmetro 'original' é obrigatório e deve ser uma string válida"
+ *     tags:
+ *       - "URLs Encurtados"
+ */
 urlsRoutes.post('/', createShortenedUrlFactory().handle)
 
+/**
+ * @swagger
+ * /{short}:
+ *   get:
+ *     summary: "Redireciona para um URL encurtado"
+ *     description: "Dado um URL encurtado, redireciona para o URL original."
+ *     parameters:
+ *       - name: short
+ *         in: path
+ *         description: "Código do URL encurtado"
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: "Redireciona para o URL encurtado"
+ *       404:
+ *         description: "Short expirado ou não localizado"
+ *     tags:
+ *       - "URLs Encurtados"
+ */
 urlsRoutes.get('/:short', getShortenedUrlFactory().handle)
 
 export { urlsRoutes }
