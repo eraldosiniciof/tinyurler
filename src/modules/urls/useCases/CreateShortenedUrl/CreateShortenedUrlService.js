51ae0631ac57e9f0cd7248ae110c4cacfba1231c
Eraldo Sinicio <eraldosinicio@gmail.com>
2024-12-28 18:14:44 -0300

    fix: ensure url text to http prefix

--- src/modules/urls/useCases/CreateShortenedUrl/CreateShortenedUrlService.js
+++ src/modules/urls/useCases/CreateShortenedUrl/CreateShortenedUrlService.js
@@ -17,11 +17,18 @@ class CreateShortenedUrlService {
   async execute(params) {
     const now = dayjs()
 
+    function ensureHttp(url) {
+      if (!url.startsWith('http') && !url.startsWith('https')) {
+        url = 'http://' + url
+      }
+      return url
+    }
+
     const shortedUrl = await this.generateShortedUrlProvider.execute(params)
     const expiresAt = dayjs(now).add(this.expiresMinutes, 'minutes').format()
 
     const created = await this.urlsRepository.create({
-      original: params.original,
+      original: ensureHttp(params.original),
       short: shortedUrl,
       created_at: now.format(),
       expires_at: expiresAt
